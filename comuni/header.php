ec4ae8ab70356773e02770b05d9f1faf0f419de2
Juan <juancruzbarrionu98@gmail.com>
2019-09-23 08:51:37 +0200

    ee

--- comuni/header.php
+++ comuni/header.php
@@ -1,8 +1,8 @@
 <html>
 <head>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<link rel="stylesheet" href="/Progetto/comuni/libs/jquery-ui.css">
+<script src="/Progetto/comuni/libs/jquery-3.4.1.min.js"></script>
+<script src="/Progetto/comuni/libs/jquery-ui.js"></script>
 </head>
 <body>
 <?php
